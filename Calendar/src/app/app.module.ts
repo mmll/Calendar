be8102972354c67d4c59b9b929230d16065e7523
lemonie <lemonie.li@emc.com>
2019-03-11 18:11:47 +0800

    update dialog

--- Calendar/src/app/app.module.ts
+++ Calendar/src/app/app.module.ts
@@ -3,27 +3,34 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './page/calendar/calendar.component';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { NewDialogComponent } from './component/new-dialog/new-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    DashboardComponent
+    DashboardComponent,
+    NewDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatDialogModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[NewDialogComponent]
 })
 export class AppModule { }
--- Calendar/src/app/page/dashboard/dashboard.component.ts
+++ Calendar/src/app/page/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material';
+import {NewDialogComponent} from '../../component/new-dialog/new-dialog.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+  }
+  openDialog(){
+    const dialogRef = this.dialog.open(NewDialogComponent)
+    dialogRef.afterClosed().subscribe(()=>{
+
+    })
   }
 
 }
